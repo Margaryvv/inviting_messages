4f91bc8e7fa3112cd235eb0d7facc67581b7719d
Andrey Kovalsky <126275987+Margaryvv@users.noreply.github.com>
2023-02-24 19:34:16 +0200

    Create UI_interface.vue

---
+++ ProjectHub/UI_interface.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="services">
+        <p>
+        &nbsp;interface, control_buttons, settings
+        &quot;Алкотек&quot;, смотрите ниже.
+        </p>
+        
+        
+<div class="services-table-title">
+        list of people who inwited
+</div>
+
+<ul>
+            <span>chat_viev</span>
+            <li>«loading»</li>
+            <li>account basic information/li>
+            <li>(056) 778-24-68, 370-55-73</li>
+</ul>
